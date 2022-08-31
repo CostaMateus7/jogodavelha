4e36dad84ea2a4b10963eaacedeb847c555a48d1
Mateus Costa <mateusscosta7@gmail.com>
2022-08-31 10:44:48 -0300

    Adicionando  jogo automatico

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let resultado = document.querySelector('#resultado')
 const conteiner = document.querySelector('#conteiner')
 const resul = document.querySelector('#resul')
 let confereEmpate = 0
+let encerrar = false
 
 let valor = "O"
 const vencedor = [
@@ -18,19 +19,38 @@ const vencedor = [
 ]
 casas.forEach((casa)=>(casa.addEventListener('click', jogada)))
 
-function jogada(e){
+ function jogada(e){
   valor = valor === "X" ? "O" : "X"
   confereEmpate ++  
   e.target.innerHTML = valor 
   e.target.classList.add(valor)
   e.target.removeEventListener('click', jogada)
   checarVencedor(valor)
-  
+  setTimeout(computador,1000)
+}
+function computador(){
+  const casasDisponiveis = []
+  for (let index = 0; index < casas.length; index++){
+    if(
+      !casas[index].classList.contains('X') && 
+      !casas[index].classList.contains('O')
+    ){
+      casasDisponiveis.push(casas[index])
+    }
+  }
+  const numeroAleatorio = Math.floor(Math.random() * casasDisponiveis.length)
+  valor = valor === "X" ? "O" : "X"
+  confereEmpate ++  
+  if(encerrar === false){
+  casasDisponiveis[numeroAleatorio].innerHTML = valor 
+  casasDisponiveis[numeroAleatorio].classList.add(valor)
+  casasDisponiveis[numeroAleatorio].removeEventListener('click', jogada)
+  checarVencedor(valor)
+  }
+
 }
 
 function checarVencedor(jogadorDaVez){
-    
-
   const ganhador = vencedor.some((comb)=>{
     return comb.every((index)=>{
       return casas[index].classList.contains(jogadorDaVez)   
@@ -43,13 +63,14 @@ function checarVencedor(jogadorDaVez){
       }
 }
 
-function teste(){
+function reiniciarJogo(){
   location.reload()
 }
 function encerrarJogo(ganhador_ou_empate){
+  encerrar = true
   resultado.style.display = 'block';
   conteiner.style.filter = 'blur(30px)';
   resul.innerHTML += `${ganhador_ou_empate} Venceu!`
   const ok = document.getElementById('ok')
-  ok.addEventListener('click', teste)     
+  ok.addEventListener('click', reiniciarJogo)     
 }
